303718b569fc33b1d4197eef15b48e6dffd163cb
rdattatr <dattatreya.radjiv@gmail.com>
2025-09-05 19:19:07 +0500

    feat: added first handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"wheres-my-pizza/internal/adapters/handlers"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	http.HandleFunc("POST	/orders", handlers.PostOrder)
+	http.ListenAndServe("localhost:8080", nil)
 }
--- internal/adapters/handlers/handlers.go
+++ internal/adapters/handlers/handlers.go
@@ -1,1 +1,8 @@
 package handlers
+
+import "net/http"
+
+func PostOrder(w http.ResponseWriter, r *http.Request) {
+	hello := []byte("hello")
+	w.Write(hello)
+}
