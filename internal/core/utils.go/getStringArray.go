4048d53b41fe15abd7802e3b6cd5472cad420210
rdattatr <dattatreya.radjiv@gmail.com>
2025-09-19 19:50:00 +0500

    refactor

--- internal/core/utils.go/getStringArray.go
+++ internal/core/utils.go/getStringArray.go
@@ -1,15 +1,12 @@
 package utils
 
 import (
-	"fmt"
 	"strings"
 )
 
 func GetStringArray(str string) []string {
 	var arr []string
 	str = strings.ReplaceAll(str, " ", "")
-	fmt.Println(str)
 	arr = strings.Split(str, ",")
-	fmt.Println(arr)
 	return arr
 }
